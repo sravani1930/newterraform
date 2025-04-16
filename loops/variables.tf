8d2dee5b51e58dadb465672018319ff48ae453af
sravani192019 <sravani.jamithireddy@gmail.com>
2025-04-17 03:42:30 +0530

    terraform

---
+++ loops/variables.tf
@@ -0,0 +1,21 @@
+variable instance_name {
+    type = list
+    default = ["mongodb","web","redis","cart","user","shipping","rabbitmq","payment","mysql","dispatch","catalogue"]
+}
+
+variable "ami_id" {
+    type    = string
+    default = "ami-0b4f379183e5706b9"
+}
+variable "instance_type" {
+    type    = string
+    default = "t3.micro"
+}
+variable "tags" {
+    type    = map
+    default = {
+     name = "web"
+     project = "ROBOSHOP"
+     Environment = "dev"
+    }
+}
