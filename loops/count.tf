613f5e033c984b170f3cb5a1d8821c5cff0be042
sravani192019 <sravani.jamithireddy@gmail.com>
2025-04-17 16:15:57 +0530

    terraform

--- loops/count.tf
+++ loops/count.tf
@@ -1,10 +1,19 @@
 resource "aws_instance" "web" {
   count = 11 #count.index is a special variable
   ami           = var.ami_id
-  instance_type = var.instance_type
+  instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "mysql" ||var.instance_names[count.index] == "shipping" ? "t3.small" : "t2.micro"
   #vpc_security_group_ids = [aws_security_group.roboshop_all.id]
   tags = {
     Name = var.instance_names[count.index]
   }
 
+}
+
+resource "aws_route53_record" "www" {
+  count = 11
+  zone_id = var.zone_id
+  name    = "${var.instance_names[count.index]}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
 }
--- loops/variables.tf
+++ loops/variables.tf
@@ -7,15 +7,18 @@ variable "ami_id" {
     type    = string
     default = "ami-0b4f379183e5706b9"
 }
-variable "instance_type" {
-    type    = string
-    default = "t3.micro"
-}
-# variable "tags" {
-#     type    = map
-#     default = {
-#      name = "web"
-#      project = "ROBOSHOP"
-#      Environment = "dev"
-#     }
+# variable "instance_type" {
+#     type    = string
+#     default = "t3.micro"
 # }
+
+variable "zone_id" {
+    
+    default = "Z0862400303HNG4OZCQC3"
+}
+
+variable "domain_name" {
+    
+    default = "devopsify.space"
+}
+
