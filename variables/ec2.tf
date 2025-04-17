33ed52cc2a08c17950680ed8fe269797dec67cea
sravani192019 <sravani.jamithireddy@gmail.com>
2025-04-17 16:16:13 +0530

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.roboshop_all.id]
+  # vpc_security_group_ids = [aws_security_group.roboshop_all.id]
   
 
   tags = var.tags
@@ -9,29 +9,29 @@ resource "aws_instance" "web" {
 }
 
 
-resource "aws_security_group" "roboshop_all" {
-  name        = var.sg_name
-  description = var.sg_description
-      ingress {
-    from_port        = var.inbound_from_port
-    to_port          = 0
-    protocol         = "tcp"
-    cidr_blocks      = var.cidr_blocks
+# resource "aws_security_group" "roboshop_all" {
+#   name        = var.sg_name
+#   description = var.sg_description
+#       ingress {
+#     from_port        = var.inbound_from_port
+#     to_port          = 0
+#     protocol         = "tcp"
+#     cidr_blocks      = var.cidr_blocks
   
- }
+#  }
     
-    egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = var.cidr_blocks
+#     egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = var.cidr_blocks
     
-  }
+#   }
 
 
 
-  tags = {
-    Name = "roboshop_all_aws"
-  }
-}
+#   tags = {
+#     Name = "roboshop_all_aws"
+#   }
+# }
 
